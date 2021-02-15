da6e4c8cc8ecadc4a96d95bd4b394a93816d6e1e
terrance weeden <terranceweeden@gmail.com>
2021-02-15 07:10:35 +0000

    add ? to one of the question

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,11 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ", "True or False: 5000 meters =  5 kilometers. ", "(5+3)/2*10 = ? ", "Given the arry[8, 'Orbit', 'Trajectory', 45]. what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ", 
+                 "True or False: 5000 meters =  5 kilometers? ", 
+                 "(5+3)/2*10 = ? ", 
+                 "Given the arry[8, 'Orbit', 'Trajectory', 45]. what entry is at index 2? ", 
+                 "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 let grade = 0;
@@ -16,7 +20,7 @@ let grade = 0;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("What is your name: ");
+  candidateName = input.question("What is your name?: ");
 }
 
 function askQuestion() {
